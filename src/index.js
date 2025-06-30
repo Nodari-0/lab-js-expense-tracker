32b0ed5ac69478e0befaf517a84861a0981ac976
Nodari-0 <nodarirusishvililinkedin@gmail.com>
2025-06-30 16:25:35 +0200

    done with classes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,71 @@
 // Entry
-class Entry {}
+class Entry {
+    constructor(date, amount, description) {
+        this.date = date;
+        this.amount = amount;
+        this.description = description;
+    }
+
+    getFormattedAmount() {
+        return `${this.amount.toFixed(0)} €`;
+    }
+}
 
 // Income
-class Income {}
+class Income extends Entry {
+    constructor(date, amount, description) {
+        super(date, amount, description);
+        this.type = "income";
+    }
+}
 
 // Expense
-class Expense {}
+class Expense extends Entry {
+    constructor(date, amount, description, paid) {
+        super(date, amount, description );
+        this.paid = paid; // Default value for expense
+        this.type = "expense";
+    }
+
+    getFormattedAmount() {
+        return `-${super.getFormattedAmount()}`;
+    }
+}
 
 // Budget
-class Budget {}
+class Budget {
+    constructor() {
+        this.entries = [];
+    }
+
+    addEntry(entry) {
+        if (entry instanceof Entry) {
+            this.entries.push(entry);
+        } else {
+            throw new Error("Invalid entry type");
+        }
+
+    }
+
+    getCurrentBalance() {
+        return this.entries.reduce((total, entry) => {
+            if (entry instanceof Income) {
+                return total + entry.amount;
+            } else if (entry instanceof Expense) {
+                return total - entry.amount;
+            }
+            return total;
+        }, 0);
+    }
+
+    getFormattedEntries() {
+        return this.entries.map(entry => {
+            if (entry instanceof Income) {
+                return `${entry.date} | ${entry.description} | ${entry.amount.toFixed(0)} €`;
+            } else if (entry instanceof Expense) {
+                return `${entry.date} | ${entry.description} | -${entry.amount.toFixed(0)} €`;
+            }
+            return '';
+        });
+    }
+}
